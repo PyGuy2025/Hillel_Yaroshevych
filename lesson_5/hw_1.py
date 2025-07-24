9063953821c6e7e96770899c700ee85115c1d896
PyGuy2025 <coolguyinweb@gmail.com>
2025-07-24 21:15:41 +0300

    edited HW 5.1

--- lesson_5/hw_1.py
+++ lesson_5/hw_1.py
@@ -4,14 +4,14 @@ from string import punctuation
 symb_list = list(punctuation.replace('_', '') + ' ')
 numbers = list(range(0, 10))
 
-users_hash = input("Print your hashtag: ")
+user_input = input("Print your hashtag: ")
 
 if (
-    all(not c.isupper() for c in users_hash) # check uppers, but keep in mind '_'
-    and users_hash not in kwlist # check system words
-    and not users_hash[0].isdigit() # check first symbol for digit reqs
-    and not any(el in users_hash for el in symb_list) # check punctuation
-    and "__" not in users_hash # check for double underscores
+    all(not c.isupper() for c in user_input) # check uppers, but keep in mind '_'
+    and user_input not in kwlist # check system words
+    and not user_input[0].isdigit() # check first symbol for digit reqs
+    and not any(el in user_input for el in symb_list) # check punctuation
+    and "__" not in user_input # check for double underscores
 ):
     print(True)
 else:
