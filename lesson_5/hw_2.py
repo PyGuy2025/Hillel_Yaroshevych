76c870847d0e2ede14f0673d5d5d60e11fd70520
PyGuy2025 <coolguyinweb@gmail.com>
2025-07-25 20:20:09 +0300

    HW 5.2

---
+++ lesson_5/hw_2.py
@@ -0,0 +1,32 @@
+import sys
+
+calc_status = True
+
+while calc_status:
+    first_num = float(input("Start with the first number: "))
+    op = input("Choose your operator: '+', '-', '*', '/': ")
+    second_num = float(input("And your second number: "))
+
+    if op == '+':
+        result = first_num + second_num
+    elif op == '-':
+        result = first_num - second_num
+    elif op == '*':
+        result = first_num * second_num
+    elif op == '/':
+        if second_num == 0:
+            print("Sorry, but you can't divide by zero")
+            result = None
+        else:
+            result = first_num / second_num
+    else:
+        print("Sorry, but probably you didn't input numbers or right operator")
+        result = None
+
+    if result is not None:
+        print(f"Your answer is: {result}")
+
+    next_input = input("Do you want to continue? (y/n): ")
+    if next_input == 'n':
+        print("See you soon. Goodbye!")
+        calc_status = False
