aa022d2430241e31633903652067c421f599a197
PyGuy2025 <coolguyinweb@gmail.com>
2025-07-30 13:15:40 +0300

    Lesson 7, updated HW_1 & HW_2

--- lesson_7/hw_2.py
+++ lesson_7/hw_2.py
@@ -4,3 +4,10 @@ def correct_sentence(text: str) -> str:
         return result
     else:
         return result + '.'
+
+assert correct_sentence("greetings, friends") == "Greetings, friends.", 'Test1'
+assert correct_sentence("hello") == "Hello.", 'Test2'
+assert correct_sentence("Greetings. Friends") == "Greetings. Friends.", 'Test3'
+assert correct_sentence("Greetings, friends.") == "Greetings, friends.", 'Test4'
+assert correct_sentence("greetings, friends.") == "Greetings, friends.", 'Test5'
+print('ОК')
