ddbc4b8125a4dc7569d2151f20c3ce4bedddcf19
PyGuy2025 <coolguyinweb@gmail.com>
2025-08-03 16:53:31 +0300

    Edited HW_3

--- lesson_7/hw_3.py
+++ lesson_7/hw_3.py
@@ -1,19 +1,13 @@
-def second_index(text:str, some_str:str) -> int or None:
-    match_count = 0
-    for elem in range(len(text) - len(some_str) + 1):
-        if text[elem: elem + len(some_str)] == some_str:
-            match_count += 1
-            if match_count == 2:
-                return elem
-    return None
+def second_index(text:str, some_str:str) -> int | None:
+    first_index = text.find(some_str)
+    if first_index != -1:
+        second_index = text.find(some_str, first_index + 1)
+        if second_index != -1:
+            return second_index
+
 
 assert second_index("sims", "s") == 3, 'Test1'
 assert second_index("find the river", "e") == 12, 'Test2'
 assert second_index("hi", "h") is None, 'Test3'
 assert second_index("Hello, hello", "lo") == 10, 'Test4'
 print('ОК')
-
-
-
-
-
